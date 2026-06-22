3edb8025d2b337ab75c5e1e2ae91ff1d778a35a8
Ömer Faruk <omerakcay490@gmail.com>
2026-06-22 20:23:43 +0300

    chore: remove stop-lingua.bat

--- stop-lingua.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Lingua durduruluyor...
-taskkill /FI "WINDOWTITLE eq Lingua Backend*" /T /F >nul 2>&1
-taskkill /FI "WINDOWTITLE eq Lingua Frontend*" /T /F >nul 2>&1
-echo Durduruldu.
-timeout /t 2 /nobreak >nul
